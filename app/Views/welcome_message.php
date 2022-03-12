29838a6fff345278e88f4331827235f98ec30739
YuraProjects <rnidevon@gmail.com>
2022-03-12 15:22:54 +0800

    first commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,7 @@
+<?php
+echo view('layout/v_header.php');
+echo view('layout/v_navbar.php');
+echo view('layout/content.php');
+echo view('layout/v_sidebar.php');
+echo view('layout/v_footer.php');
+echo view('layout/v_js.php');
